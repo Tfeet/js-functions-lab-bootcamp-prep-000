fae80d05de9e879e95f83370986ac8e91c6600ae
Rebecca Sorrell <rebeccassorrell@gmail.com>
2021-01-09 01:27:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code below!
+function happyHolidays(string){
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name){
+  return (`Happy holidays, ${name}!`)
+}
+function happyCustomHolidaysTo(holiday,name),
+ console.log`Happy ${happyHolidays}, ${happyHolidaysTo}!`
+ return ("Happy Independence Day To You")
+}
+function holidayCountdown(holiday, days){
+  console.log(`It's ${days} days until ${holiday}!`)
+  return("It's 120 days until Father's Day!")
+}
